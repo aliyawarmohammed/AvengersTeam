19e4acfa714b7db2329f4c174b14c4f7fc46f80d
Dell <Dell@DESKTOP-3T7SP2U>
2024-10-21 21:34:40 +0530

    committed

---
+++ DoomsDay/DelegateExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomsDay
+{
+
+    delegate void ShowScreen();
+    internal class DelegateExample
+    {
+        public void Show()
+        {
+
+            Console.WriteLine("Hi am JAck");
+        }
+
+        public void Showchan()
+        {
+
+            Console.WriteLine("Hi am chan");
+        }
+
+        public static void Main()
+        {
+            DelegateExample obj = new DelegateExample();
+            ShowScreen objdel = new ShowScreen(obj.Show);
+            objdel += obj.Showchan;
+            //objdel.Invoke();
+            Console.ReadLine();
+
+        }
+
+    }
+}
+
+
+//Delegate is similar to a function pointer in C++
---
+++ DoomsDay/MyExceptionExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomsDay
+{
+    internal class MyExceptionExample
+    {
+
+        public static void Main()
+        {
+            try
+            {
+                int num = 0;
+
+                int c = num / 2;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("hi");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
