91af97df7d68846212e6bc7a367639397dd573ed
Dell <Dell@DESKTOP-3T7SP2U>
2024-10-30 21:28:12 +0530

    committed code

--- DoomsDay/MultiThreadExample.cs
+++ DoomsDay/MultiThreadExample.cs
@@ -12,19 +12,31 @@ namespace DoomsDay
 
         public static void Thread1()
         {
+            Thread t = Thread.CurrentThread;
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(t.Name+" running "+i);
             }
         }
 
         public static void Thread2()
         {
+            Thread t = Thread.CurrentThread;
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(t.Name + " running " + i);
             }
         }
+
+        public static void Thread3()
+        {
+            Thread t = Thread.CurrentThread;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(t.Name + " running " + i);
+            }
+        }
+
 
 
     }
@@ -34,13 +46,47 @@ namespace DoomsDay
     {
         public static void Main()
         {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Main thread";
+            Console.WriteLine(t.Name+" is running thread");
+
+
+
+
+
+
             Thread t1 = new Thread(new ThreadStart(MultiThreadExample.Thread1));
-            t1.Start();
-            t1.Suspend();//Thread.Sleep(4000);
-
             Thread t2 = new Thread(new ThreadStart(MultiThreadExample.Thread2));
-            t2.Start();
+            Thread t3 = new Thread(new ThreadStart(MultiThreadExample.Thread3));
 
+            t1.Name = "printing english paper";
+            t2.Name = "printing hindi paper";
+            t3.Name = "printing telgue paper";
+
+
+            t1.Priority = ThreadPriority.Lowest;
+            t2.Priority = ThreadPriority.Normal;
+            t3.Priority = ThreadPriority.Highest;
+
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+
+            // t1.Suspend();//Thread.Sleep(4000);
+            //t1.Join();//join method will not allow another thread to execute untill t1 complete execution
+
+
+
+
+
+
+          
+
+            //Thread.Sleep(4000);
+            // t1.Resume();
+            //t2.Abort();
             Console.ReadLine();
 
         }
