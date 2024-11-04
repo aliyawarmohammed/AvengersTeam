1b30ee578e233f75ff8426531300fe1c04c65b23
Dell <Dell@DESKTOP-3T7SP2U>
2024-11-04 21:16:20 +0530

    committed

---
+++ DoomsDay/FilinfoExample.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomsDay
+{
+    internal class FilinfoExample
+    {
+
+        public static void Main()
+        {
+			try
+			{
+
+                //beging creation of file
+                //string path = "D:\\FirstFile.txt";
+
+                //FileInfo fi = new FileInfo(path);
+                //fi.Create();
+                //Console.WriteLine("File is Created");
+                ///Ending creation file
+                ///
+
+
+                //writing data inside file
+                string path = "D:\\First";
+
+                FileInfo fi = new FileInfo(path);
+                //StreamWriter sw = fi.CreateText();
+                //sw.WriteLine("Hello guys ");
+                //sw.Close();
+                //////////////////////
+                ///
+
+
+                //StreamReader sr=fi.OpenText();
+
+                //string output ="";
+                //while ((output=sr.ReadLine())!=null)
+                //{
+
+                //    Console.WriteLine(output);
+                
+                //}
+
+
+
+                DirectoryInfo directory=new DirectoryInfo(path);
+
+
+                directory.Delete();
+                Console.WriteLine("diector crated ");
+
+
+
+                Console.ReadLine();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				throw;
+			}
+        }
+    }
+}
